4cbd01e136c7f08b15a13373a3338bd262c30c0d
Dean Alan Summer <deanlovesmargie@gmail.com>
2021-12-26 16:00:02 -0600

    Create TypeAnswer.js

---
+++ src/question-types/TypeAnswer.js
@@ -0,0 +1,13 @@
+class TypeAnswerQuestion extends QuestionBase {
+  constructor(question, correctAnswers, afterCheckFunction) {
+    const checkButton = document.body.createElement("button")
+    const userAnswerInput = document.body.createElement("textarea")
+    const userAnswer = userAnswerInput.innerText
+    
+    super(question, correctAnswers, true, userAnswer, checkButton)
+    
+    this.userAnswerInput = userAnswerInput
+    
+    this.checkButton.addEventListener("click", this => afterCheckFunction)
+  }
+}
