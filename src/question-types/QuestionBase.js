a7bbaf054185f9c86eccc9b3ab250f087d517017
Dean Alan Summer <deanlovesmargie@gmail.com>
2021-12-26 14:40:21 -0600

    Created a base for question types
    The QuestionBase class allows questions to be created for Oxese lessons in this project. All the question types extend off of this base class.

---
+++ src/question-types/QuestionBase.js
@@ -0,0 +1,28 @@
+class QuestionBase {
+  constructor(question, correctAnswer, multipleCorrectAnswers, userAnswer, checkButton, afterCheckFunction) {
+    this.question = question
+    this.correctAnswer = correctAnswer
+    this.multipleCorrectAnswers = Boolean(multipleCorrectAnswers)
+    this.userAnswer = userAnswer
+    this.checkButton = checkButton
+    this.afterCheckFunction = afterCheckFunction
+    this.isCorrect = false
+    this.buttonClicked = false
+    
+    this.checkButton.addEventListener("click", () => {
+      if (!this.buttonClicked) {
+        if (this.multipleCorrectAnswers) {
+          this.correctAnswer.forEach(answer => {
+            if (answer === this.userAnswer) return this.isCorrect = true
+          })
+        } else {
+          if (this.correctAnswer === this.userAnswer) this.isCorrect = true
+        }
+      }
+      
+      this.buttonClicked = true
+      
+      this.afterCheckFunction(this)
+    })
+  }
+}
